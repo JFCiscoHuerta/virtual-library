4187307e23a9eb55d9767648553ee7b76f138a09
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-17 23:17:04 -0600

    project setup

---
+++ src/test/java/com/gklyphon/VirtualLibrary/VirtualLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gklyphon.VirtualLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
